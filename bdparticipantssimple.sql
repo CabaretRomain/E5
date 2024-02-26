75dc41d4fe42e949806aa6b9c594e5c70ca55fe0
Rick <JeanBleu@gmail.com>
2024-02-26 14:37:23 +0100

    adding bdd

--- bdparticipantssimple.sql
+++ bdparticipantssimple.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : ven. 09 fév. 2024 à 08:40
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Host: localhost:3306
+-- Generation Time: Feb 26, 2024 at 01:35 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,65 +18,93 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `bdparticipantssimple`
+-- Database: `bdparticipantssimple`
 --
-CREATE DATABASE IF NOT EXISTS `bdparticipantssimple` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `bdparticipantssimple`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `participant`
+-- Table structure for table `participant`
 --
 
-DROP TABLE IF EXISTS `participant`;
-CREATE TABLE IF NOT EXISTS `participant` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `participant` (
+  `id` int NOT NULL,
   `nom` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `mail` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `balance` int DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `categorie` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `participant`
+-- Dumping data for table `participant`
 --
 
-INSERT INTO `participant` (`id`, `nom`, `mail`, `balance`) VALUES
-(1, 'p1', 'mail1', 200),
-(2, 'p2', 'mail2', 0),
-(3, 'p3', 'mail3333', 0),
-(4, 'noivNom', 'nouvMail', 200),
-(5, 'p5', 'mail5', 200),
-(11, 'p6', 'm6', 0),
-(12, 'p7', 'm7', 200),
-(13, 'p8', 'm8', 2000);
+INSERT INTO `participant` (`id`, `nom`, `mail`, `balance`, `categorie`) VALUES
+(1, 'p1', 'mail1', 200, 'famille'),
+(2, 'p2', 'mail2', 0, 'professionelle'),
+(3, 'p3', 'mail3333', 0, 'ami'),
+(4, 'noivNom', 'nouvMail', 200, 'famille'),
+(5, 'p5', 'mail5', 200, 'professionelle'),
+(11, 'p6', 'm6', 0, 'enfant'),
+(12, 'p7', 'm7', 200, 'famille'),
+(13, 'p8', 'm8', 2000, 'enfant');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateur` (
+  `id` int NOT NULL,
   `nom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `passe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `typeU` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `passeHash` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `passeHash` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `utilisateur`
+-- Dumping data for table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id`, `nom`, `passe`, `typeU`, `passeHash`) VALUES
 (6, 'aa', 'aa', 'A', '1b86355f13a7f0b90c8b6053c0254399994dfbb3843e08d603e292ca13b8f672ed5e58791c10f3e36daec9699cc2fbdc88b4fe116efa7fce016938b787043818'),
 (7, 'pp', 'pp', 'P', 'ad0687d00b0fd6127f31710056990be87d3362982fccddcf40aef2eb1da5c4f468063bcca53d61c18d3b6c5acfbccf7a6f0b9b75a67b684d0570918d491ea1f8'),
 (9, 'nomX', 'pX', 'A', '3bcb9480ff31d9dfff57a0b03d7d74f2f61d511c14222bb81e50be7e01f9ed25aa812ef781b706566ac9867773285a1d1f45b1ca041a29cfcad0622c7336a7c8');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `categorie` (`categorie`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `participant`
+--
+ALTER TABLE `participant`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
